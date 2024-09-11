0d2fa781519f475fa48b58adce59e7a91088c215
Rohit Kumar Yadav <rohitkumaryadav2616@gmail.com>
2024-09-12 01:44:40 +0530

    mongo uri

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connection = () =>
   mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect("mongodb://127.0.0.1:27017/zithasTaskManagementApp")
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("Error Connecting with MongoDB:", err));
 
